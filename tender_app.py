a4577ae6c481de4d72060f822a62ed669755665a
Rashad Gasimzada <liamatux@tuta.io>
2026-08-14 07:44:09 +0000

    tender_app copy file deleted

--- tender_app.py
+++ tender_app.py
@@ -276,11 +276,6 @@ HTML_PAGE = """<!DOCTYPE html>
 </body>
 </html>"""
 
-def open_browser():
-    import time
-    time.sleep(1)
-    webbrowser.open('http://127.0.0.1:5000')
 
 if __name__ == '__main__':
-    threading.Thread(target=open_browser, daemon=True).start()
     app.run(host='0.0.0.0', debug=False, port=5000)
